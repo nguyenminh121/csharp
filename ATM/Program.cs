a6ac71a36c94981c0abc00981aa42901e31c4f2f
nguyenminh121 <ngthminh121@gmail.com>
2026-02-01 23:20:38 +0700

    first cm

---
+++ ATM/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the ATM!\n");
+            Console.WriteLine("Please providing your information");
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter your account number:");
+            string accountNumber = Console.ReadLine();
+            Console.WriteLine("Enter your first balance:");
+            double balance = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please select an option:\n");
+            Console.WriteLine("1. Withdraw\n2. Deposit\n3. View Balance\n");
+            int options;
+            Console.WriteLine("Please select an option:");
+            options = int.Parse(Console.ReadLine());
+            switch (options)
+            {
+                case 1:
+                    Console.WriteLine("Withdraw");
+                    double withdrawAmount;
+                    Console.WriteLine("Enter amount to withdraw:");
+                    withdrawAmount = double.Parse(Console.ReadLine());
+                    if (withdrawAmount > balance)
+                    {
+                        Console.WriteLine("Insufficient funds.");
+                    }
+                    else
+                    {
+                        balance -= withdrawAmount;
+                        Console.WriteLine("Withdrawal successful. New balance: " + balance);
+                    }
+                    Console.WriteLine("Your current balance is: " + balance);
+                    break;
+                case 2:
+                    Console.WriteLine("Deposit");
+                    double depositAmount;
+                    Console.WriteLine("Enter amount to deposit:");
+                    depositAmount = double.Parse(Console.ReadLine());
+                    balance += depositAmount;
+                    Console.WriteLine("Deposit successful. New balance: " + balance);
+                    break;
+                default:
+                    Console.WriteLine("View Balance");
+                    Console.WriteLine("Your current balance is: " + balance);
+                    break;
+            }
+        }
+    }
+}
